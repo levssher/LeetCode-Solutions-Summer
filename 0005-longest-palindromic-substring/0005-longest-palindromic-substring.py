fd1dbe9c58ed445e5ec37d274ac3a7c8525e438f
Harzinberg <levssher@gmail.com>
2026-08-04 00:14:13 +0300

    Time: 218 ms (90.47%), Space: 19.4 MB (17.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,13 +1,10 @@
 class Solution:
     def checkTheString(self, s: str, left:int, right: int):
         len_s=len(s)
-        result=""
         while(left>=0 and right<= len_s-1 and s[left]==s[right]):
-                if right-left+1>len(result):
-                    result=s[left:right+1]
                 left-=1
                 right+=1
-        return result        
+        return s[left + 1 : right]        
 
     def longestPalindrome(self, s: str) -> str:
         len_s=len(s)
